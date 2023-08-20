4207ce0e8a348fc545b5cd0a490a4d2e279fd649
carolina <carolraul27@gmail.com>
2023-08-20 16:53:21 -0400

    created new php docs for each page and new css doc

---
+++ create_new_pass.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    
+    <body>
+        <h1>Enter new password to store: </h1>
+            <form action='connect.php' method="POST">
+                <label for="password">New Password: </label><br>
+                <input type='text' name='password' id='password' required/> <br><br>
+            </form>
+        <?php
+         echo "hello world people life";
+        ?>
+    </body>
+</html>
