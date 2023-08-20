8588e9d73c9c6ac43546485ba3424fa5260d85a3
carolina <carolraul27@gmail.com>
2023-08-20 15:50:28 -0400

    Created DB and HTML form

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     </head>
     
     <body>
+        <h1>Enter new password to store: </h1>
+            <form action='connect.php' method="POST">
+                <label for="password">New Password: </label><br>
+                <input type='text' name='password' id='password' required/> <br><br>
+            </form>
         <?php
-        echo "hello world people life";
+         echo "hello world people life";
         ?>
     </body>
 </html>
