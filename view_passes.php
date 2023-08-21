64cdd33912af2f6fcdcb990b12d8c6958bca111d
carolina <carolraul27@gmail.com>
2023-08-21 16:25:16 -0400

    addded topnav div to body

--- view_passes.php
+++ view_passes.php
@@ -3,11 +3,13 @@
     <head>
       <link rel="stylesheet" href="styling.css">
     </head>
+    
+    <body>
     <div class="topnav">
         <a href='main.php'>Home</a>
     </div>
-    <body>
-      <h1>View your passwords</h1>
+    
+    <h1>View your passwords</h1>
       
         <?php
         ?>
