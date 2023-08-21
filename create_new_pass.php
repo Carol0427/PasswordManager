ca1533f8f1b8f3ec9c382e8afa0b657826a31df0
carolina <carolraul27@gmail.com>
2023-08-21 16:00:30 -0400

    fixed errors cuz accidentally deleted input boxes

--- create_new_pass.php
+++ create_new_pass.php
@@ -6,8 +6,13 @@
     <body>
         <h1>Enter new password to store: </h1>
             <form action='connect.php' method="POST">
+                <label for="site">Site name: </label> <br>
+                <input type='text' name='site' id='site' required/> <br><br>
+                
                 <label for="password">New Password: </label><br>
                 <input type='text' name='password' id='password' required/> <br><br>
+
+                <input type="submit" name="new_pass_submit" id="new_pass_submit"/>
             </form>
         <?php
          echo "hello world people life";
