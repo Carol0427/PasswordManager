2f20d302556a2fe6f115503e579374e29d2efe90
carolina <carolraul27@gmail.com>
2023-08-21 16:22:33 -0400

    some change

--- view_passes.php
+++ view_passes.php
@@ -3,7 +3,9 @@
     <head>
       <link rel="stylesheet" href="styling.css">
     </head>
-    
+    <div class="topnav">
+        <a href='main.php'>Home</a>
+    </div>
     <body>
       <h1>View your passwords</h1>
       
