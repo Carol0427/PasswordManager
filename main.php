4f4a86798fee7fc7c0f95750bd783c8510a3a044
carolina <carolraul27@gmail.com>
2023-08-21 16:06:44 -0400

    created main page to go to view_passes and create

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include 'connect.php';
+        ?>
+    </head>
+<body>
+<div id='main_page'>
+        <div id='create_new_pass'> <h2><a href='create_new_pass.php'>go to create passes</a></h2></div>
+        <div id='view_passes'> <h2><a href='view_passes.php'>go to view passes</a></h2></div>
+        
+</div>
+</body>
+</html>
---
+++ view_passes.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel="stylesheet" href="styling.css">
+    </head>
+    
+    <body>
+      <h1>View your passwords</h1>
+      
+        <?php
+        ?>
+    </body>
+</html>
